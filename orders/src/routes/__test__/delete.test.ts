171aa8f7132dd3f9bfdfe38d4c0d4d2862eeb3af
katonzaky <zakykaton@gmail.com>
2022-02-23 14:04:53 -0600

    Added nats publisher for order and nats listener for tickets

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { app } from '../../app';
 import { Order, OrderStatus } from '../../models/order';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 const buildTicket = async () => {
   const ticket = Ticket.build({
@@ -67,4 +68,30 @@ it('Cancels order status', async () => {
     .expect(200);
 
   expect(fetchRequest.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('emits an order cancelled event', async () => {
+  const user1 = global.signin();
+
+  const ticket = await buildTicket();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .patch(`/api/orders/${order.id}`)
+    .set('Cookie', user1)
+    .expect(204);
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user1)
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   NotAuthorizedError,
 } from '@kzticketing/common';
 import { Order, OrderStatus } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 
 const router = express.Router();
 
@@ -14,7 +16,7 @@ router.patch(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -26,6 +28,12 @@ router.patch(
     await order.save();
 
     // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
