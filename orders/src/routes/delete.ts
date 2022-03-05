0ee493230dfaf8c821cddc59925ba3f235656e19
Katon Zaky <zakykaton@gmail.com>
2022-03-05 14:36:25 -0600

    test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
 import {
-  requireAuth,
-  NotFoundError,
-  NotAuthorizedError,
+	requireAuth,
+	NotFoundError,
+	NotAuthorizedError,
 } from '@kzticketing/common';
 import { Order, OrderStatus } from '../models/order';
 import { natsWrapper } from '../nats-wrapper';
@@ -11,33 +11,34 @@ import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-pu
 const router = express.Router();
 
 router.patch(
-  '/api/orders/:orderId',
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const { orderId } = req.params;
+	'/api/orders/:orderId',
+	requireAuth,
+	async (req: Request, res: Response) => {
+		const { orderId } = req.params;
+		console.log('test');
 
-    const order = await Order.findById(orderId).populate('ticket');
+		const order = await Order.findById(orderId).populate('ticket');
 
-    if (!order) {
-      throw new NotFoundError();
-    }
-    if (order.userId !== req.currentUser!.id) {
-      throw new NotAuthorizedError();
-    }
-    order.status = OrderStatus.Cancelled;
-    await order.save();
+		if (!order) {
+			throw new NotFoundError();
+		}
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+		order.status = OrderStatus.Cancelled;
+		await order.save();
 
-    // publishing an event saying this was cancelled!
-    new OrderCancelledPublisher(natsWrapper.client).publish({
-      id: order.id,
-      version: order.version,
-      ticket: {
-        id: order.ticket.id,
-      },
-    });
+		// publishing an event saying this was cancelled!
+		new OrderCancelledPublisher(natsWrapper.client).publish({
+			id: order.id,
+			version: order.version,
+			ticket: {
+				id: order.ticket.id,
+			},
+		});
 
-    res.status(204).send(order);
-  }
+		res.status(204).send(order);
+	},
 );
 
 export { router as deleteOrderRouter };
