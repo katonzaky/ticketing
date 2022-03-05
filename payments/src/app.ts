e26c24fc8d6776dc3b8684c82bfaa9b182fa0b11
Katon Zaky <zakykaton@gmail.com>
2022-03-05 16:21:31 -0600

    Updated hostname

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -9,17 +9,17 @@ const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== 'test',
-  })
+	cookieSession({
+		signed: false,
+		secure: false,
+	}),
 );
 app.use(currentUser);
 
 app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError();
+	throw new NotFoundError();
 });
 
 app.use(errorHandler);
